3ecd7148f88e46748b4168ce4d24f64c023fcec5
Jop Verhoeven <jop.verhoeven@iodigital.com>
2022-12-06 12:23:49 +0100

    fix

--- src/pages/ScorePage.jsx
+++ src/pages/ScorePage.jsx
@@ -53,7 +53,7 @@ function ScorePage() {
 
     useEffect(() => {
         fetchGame();
-    })
+    }, [])
 
     if (error) {
         return <div className="flex flex-col bg-gradient-to-br from-indigo-500 via-purple-500 to-pink-500 text-white rounded-3xl p-4">
